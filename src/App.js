1af92c490fdb02f8982a514da82c1ba0cf2f99a8
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-07-27 14:07:03 -0500

    created note app using react hooks and speech recognition software

--- src/App.js
+++ src/App.js
@@ -1,19 +1,79 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="box">
-        <h2>Current Note</h2>
-        <button>Save Note</button>
-        <button>Start/Stop</button>
-      </div>
-        <div className="box">
-          <h2>Notes</h2>
-        </div>
 
-    </div>
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const mic = new SpeechRecognition()
+
+mic.continuous = true
+mic.interimResults = true
+mic.lang = 'en-US'
+
+function App() {
+
+  const [isListening, setIsListening] = useState(false)
+  const [note, setNote] = useState(null)
+  const [savedNotes, setSavedNotes] = useState([])
+
+  useEffect(() => {
+    handleListen()
+  }, [isListening])
+
+  const handleListen = () => {
+    if (isListening) {
+      mic.start()
+      mic.onend = () => {
+        console.log('continue...')
+        mic.start()
+      }
+    } else {
+      mic.stop()
+      mic.onend = () => {
+        console.log('stopped mic on click...')
+      }
+    }
+
+    mic.onstart = () => {
+      console.log('mic is on')
+    }
+
+    mic.onresult = event => {
+      const transcript = Array.from(event.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('')
+      console.log(transcript)
+      setNote(transcript)
+      mic.onerror = event => {
+        console.log(event.error)
+      }
+    }
+  }
+
+  const handleSaveNote = () => {
+    setSavedNotes([...savedNotes, note])
+    setNote('')
+  }
+
+  return (
+    <>
+    <h1>Voice Notes</h1>
+      <div className="container">
+        <div className="box">
+          <h2>Current Note</h2>
+          {isListening ? <span>is listening</span> : <span>is not listening</span>}
+          <button onClick={handleSaveNote} disabled={!note}>Save Note</button>
+          <button onClick={() => setIsListening(prevState => !prevState )} >Start/Stop</button>
+          <p>{note}</p>
+        </div>
+          <div className="box">
+            <h2>Notes</h2>
+            {savedNotes.map(n => (
+              <p key={n}>{n}</p>
+            ))}
+          </div>
+      </div>
+    </>
   )
 }
 
